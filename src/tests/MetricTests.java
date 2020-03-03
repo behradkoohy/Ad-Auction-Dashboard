92efd95b5f2e06f97f1ce716d0b5a172d85df8ec
Peter Mant <44090361+PeterHMant@users.noreply.github.com>
2020-03-03 17:13:25 +0000

    making small test files

--- src/tests/MetricTests.java
+++ src/tests/MetricTests.java
@@ -1,9 +1,11 @@
-package models;
+package tests;
 
 
 import daos.ClickDao;
 import daos.ImpressionDao;
 import daos.ServerEntryDao;
+import models.Metrics;
+import models.ReaderCSV;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,7 +22,9 @@ public class MetricTests {
         impressionsDao = new ImpressionDao();
         serverDao = new ServerEntryDao();
 
-
+        ReaderCSV.readCSV("/testFiles/testClick.csv");
+        ReaderCSV.readCSV("/testFiles/testImpression.csv");
+        ReaderCSV.readCSV("/testFiles/testServer.csv");
 
 
         metrics = new Metrics(clickDao, impressionsDao, serverDao);
