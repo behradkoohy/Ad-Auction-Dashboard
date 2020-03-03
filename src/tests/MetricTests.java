1213ffed7b136ce635c37a3b2868373bed94f769
Peter Mant <44090361+PeterHMant@users.noreply.github.com>
2020-03-03 18:22:34 +0000

    fixed test files

--- src/tests/MetricTests.java
+++ src/tests/MetricTests.java
@@ -36,8 +36,40 @@ public class MetricTests {
 
     @Test
     public void totalImpressionsTest() {
+        System.out.println(metrics.getNumImpressions("test"));
         assertTrue(metrics.getNumImpressions("test") == 99);
     }
+
+    @Test
+    public void totalClicksTest() {
+        assertTrue(metrics.getNumClicks("test") == 99);
+    }
+
+    @Test
+    public void uniquesTest() {
+        assertTrue(metrics.getNumUniqs("test") == 98);
+    }
+
+    @Test
+    public void conversionsTest() {
+        assertTrue(metrics.getConversions("test") == 6);
+    }
+
+    @Test
+    public void cTRTest() {
+        assertTrue(metrics.getCTR("test") == 1);
+    }
+
+
+    //total test click cost = 464.83541
+    //total test impression cost = 0.105674
+    //total total cost = 464.941084
+    @Test
+    public void cPATest() {
+        assertTrue(metrics.getCPA("test") == (464.941084/6));
+    }
+
+
 
 
 }
