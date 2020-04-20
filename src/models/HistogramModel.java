a8c0176726243c4a36ad91c98f665e480da24496
Alex Pop <Appop99@gmail.com>
2020-04-20 15:47:28 +0100

    Initial histogram zoom functionality

--- src/models/HistogramModel.java
+++ src/models/HistogramModel.java
@@ -4,42 +4,62 @@ import daos.ClickDao;
 import daos.DaoInjector;
 import entities.Click;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class HistogramModel {
     private String campaign = null;
     private ClickDao clickDao = DaoInjector.newClickDao();
-    //For testing purposes
-    private int band_length = 1;
+    private HashMap<Double, Integer> histogramData = new HashMap<Double, Integer>();
 
     public void setCampaign(String campaign) {
         this.campaign = campaign;
     }
 
-    public List<Integer> getData() {
-        List<Integer> chartData = new ArrayList<Integer>();
-        chartData.add(0);
-
+    public HashMap<Double, Integer> getData(double minCost, Double maxCost, double bandLength ) {
         List<Click> data = clickDao.getFromCampaign(this.campaign);
+        this.histogramData.clear();
 
+        // if no max cost specified, get the hightest cost of a click
+        Double maxClickCost = (maxCost != null) ? maxCost : (Math.floor(Collections.max(data, Comparator.comparing(c -> c.getClickCost())).getClickCost()) + 2*bandLength);
+
+        // create the hashmap to hold the data
+        this.createBands(minCost, maxClickCost, bandLength);
+
+        // for each click, add it to the band
         for( Click click : data){
-            double cost = click.getClickCost();
-            if( cost > chartData.size() - 1 ){
-                chartData.add(1);
-            }else{
-                chartData = this.addToBand(chartData, cost);
+            Double cost = click.getClickCost();
+            // if click between the values of interest
+            if( cost >= minCost && cost <= maxClickCost ){
+                // get the cost with respect to its band
+                List<Double> keys = new ArrayList<Double>(this.histogramData.keySet());
+                Collections.sort(keys);
+
+                for( int i =0; i < keys.size(); i++ ){
+                    double key = keys.get(i);
+                    if( i == keys.size() - 1 ){
+                        // last band, edge case
+                        this.histogramData.put( key, this.histogramData.get(key) + 1);
+                        break;
+                    }else{
+                        if( cost >= key && cost < keys.get(i+1) ){
+                            this.histogramData.put( key, this.histogramData.get(key) + 1);
+                            break;
+                        }
+                    }
+                }
             }
         }
-
-        return chartData;
+        return this.histogramData;
     }
 
-    private List<Integer> addToBand(List<Integer> chartData, double clickCost) {
-        int band = (int) (clickCost / band_length);
-        chartData.set(band, chartData.get(band) + 1);
-        return chartData;
+    private void createBands(double min, double max, double bandLength) {
+        // first band is the min value
+        double currentBand = min;
+
+        // as long as max not reached, iterate through intermediate values and create bands
+        while (currentBand < max) {
+            this.histogramData.put(currentBand, 0);
+            currentBand = Math.round((currentBand + bandLength) * 1000) / 1000.0;
+        }
     }
-
-
 }
